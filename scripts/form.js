4660f892e5cd9072f89c8d86ba54c37b33697fc7
Kacper Chamala <98347889+Lolowyo-dev@users.noreply.github.com>
2023-03-26 17:30:53 +0200

    Add files via upload

---
+++ scripts/form.js
@@ -0,0 +1,19 @@
+function save_data() {
+    var data = {};
+    data.email = document.getElementById('inputEmail').value;
+    var name = document.getElementById('inputUser').value;
+    var names = name.split(' ');
+    data.name = names[0];
+    data.surname = names[1];
+    data.adress = document.getElementById('inputAddress').value;
+    data.city = document.getElementById('inputCity').value;
+    data.province = document.getElementById('inputProvince').value;
+    data.zip = document.getElementById('inputZip').value;
+    data.message = document.getElementById('inputMessage').value;
+    return data;
+}
+
+function collect_data() {
+    var jasonData = save_data();
+    console.log(jasonData);
+}
