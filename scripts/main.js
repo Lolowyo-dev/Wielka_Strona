b15d4f4f62a5a0ce8e35c275089b3d6b045aee3d
Kacper Chamala <98347889+Lolowyo-dev@users.noreply.github.com>
2023-04-16 13:58:24 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,18 @@ function mode(){
         document.getElementById('mode-switch').classList = "fa-solid fa-moon";
     }
 }
+
+function font(size){
+    document.querySelector("html").style.fontSize = `${size}px`;
+    }
+
+function saturate(){
+  if (document.querySelector("html").getAttribute('data-bs-saturate') == 'true') {
+      document.querySelector("html").setAttribute('data-bs-saturate','false');
+      document.querySelector("html").style.filter= "saturate(1)";
+  }
+  else {
+      document.querySelector("html").setAttribute('data-bs-saturate','true');
+      document.querySelector("html").style.filter= "saturate(0)";
+  }
+}
